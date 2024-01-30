b553ad481ae96e72738013d56c1bac061c798abd
DFLotus <deondraefrancis101@gmail.com>
2024-01-30 14:45:47 -0500

    BS-8: Connected data base and synced

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express"
+import sequelize from "./config/connection";
+import 'dotenv/config'
 
 const app: express.Application = express();
-const PORT: number = 3001;
+const PORT: number = Number(process.env.PORT) | 3001;
 
-app.listen(PORT, (): void => {
-    console.log(`Server is running on port ${PORT}`)
-});
+sequelize.sync().then((): void => {
+    app.listen(PORT, (): void => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+    .catch((error: Error): void => {
+        console.error('Error synchronizing database:', error);
+    });
