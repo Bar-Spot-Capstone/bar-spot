a527f10814580647e198db42c657b7c9a27795d1
DFLotus <deondraefrancis101@gmail.com>
2024-02-03 20:23:44 -0500

    BS-17: Added group table and patched error

---
+++ server/src/models/Group.ts
@@ -0,0 +1,26 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/connection';
+
+class Group extends Model {
+    public id!: number;
+    public name!: string;
+};
+
+Group.init(
+    {
+        id: {
+            type: DataTypes.INTEGER.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Group',
+        timestamps: true
+    }
+);
