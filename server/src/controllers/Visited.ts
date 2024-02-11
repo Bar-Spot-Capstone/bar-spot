26635aafe181b24488fd7987f7c9e3ef96355c64
SkyMazter <comunidadoscar@gmail.com>
2024-02-11 16:20:32 -0500

    created get function for visited table

--- server/src/controllers/Visited.ts
+++ server/src/controllers/Visited.ts
@@ -1,33 +1,45 @@
 import Visited from "../models/Visited";
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 
 const newVisited = async (req: Request, res: Response): Promise<Response> => {
+  const { bar_name, address }: { bar_name: string; address: string } = req.body;
 
-    
-    const {bar_name, address}: {bar_name: string, address: string} = req.body;
+  var handleEmpty: string = "";
+  handleEmpty = !bar_name ? "bar_name" : "" || !address ? "address" : "";
+  if (handleEmpty) {
+    res.status(400);
+    return res.json({
+      error: `Failed to add new entire missing field: ${handleEmpty}`,
+    });
+  }
 
-    var handleEmpty: string = '' 
-    handleEmpty = !bar_name? 'bar_name' : '' || !address? 'address' : '' 
-    if (handleEmpty) { res.status(400); return res.json({ error: `Failed to add new entire missing field: ${handleEmpty}` }) };
+  try {
+    await Visited.create({
+      bar_name: bar_name,
+      address: address,
+    });
 
-    try {
-        await Visited.create({
-            bar_name: bar_name,
-            address: address
-        });
-
-        res.status(200)
-        return res.json({ success: "Sucesfully added to Visited Table!"});
-
-        
-    }
-    catch (error: any) {
-        res.status(500);
-        return res.json({ error: `Failed to add visited bar with unexpected error: ${error}` })
-    };
+    res.status(200);
+    return res.json({ success: "Sucesfully added to Visited Table!" });
+  } catch (error: any) {
+    res.status(500);
+    return res.json({
+      error: `Failed to add visited bar with unexpected error: ${error}`,
+    });
+  }
 };
 
-export{
-    newVisited
-}
+const getAllVisited = async (req: Request, res: Response) => {
+  try {
+    const bars = await Visited.findAll();
+    res.status(200);
+    return res.json(bars);
+  } catch (error: any) {
+    res.status(500);
+    return res.json({
+      error: `Failed to retrieve visited bars with unexpected error: ${error}`,
+    });
+  }
+};
 
+export { newVisited, getAllVisited };
