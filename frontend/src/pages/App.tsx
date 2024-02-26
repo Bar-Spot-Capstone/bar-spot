5c9eb648ad799b456d05e3a6ed1adfda37f5d2fc
SkyMazter <comunidadoscar@gmail.com>
2024-02-26 11:29:25 -0500

    deleted comments

--- frontend/src/pages/App.tsx
+++ frontend/src/pages/App.tsx
@@ -1,10 +1,6 @@
 import NavBar from "../components/NavBar";
 
 const App = () => {
-  // const testStyle = {
-  //   overflow: 'auto',
-  //   height: "500px"
-  // };
   return (
     <>
       <NavBar />
