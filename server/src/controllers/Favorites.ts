a1389e3cbf31f20a79fbf8f2cceea76721c3d73f
Miles Carden <milescarden@gmail.com>
2024-02-28 16:27:20 -0500

    added controller functionality with routing for both clearFavorites and deleteFavorite

--- server/src/controllers/Favorites.ts
+++ server/src/controllers/Favorites.ts
@@ -46,7 +46,72 @@ const getFavorites = async (req: Request, res: Response) => {
     return res.json({ success: `${userId}` });
 };
 
+const deleteFavorite = async (req: Request, res: Response) => {
+    const userId: string = req.params.userId; // Takes both a userId and favorite id to decide which favorite object to destroy 
+    const id: string = req.params.id;
+
+    var handleEmpty: string = ''
+    handleEmpty = !userId ? 'userId' : '' || !id ? 'id' : '' //find missing parm
+
+    if (handleEmpty) {
+        res.status(400);
+        return res.json({ error: `Unable to read: ${handleEmpty}` })
+    };
+
+    try {
+        const favoriteRemoved: number = await Favorites.destroy({
+            where: {
+                id: id,
+                userId: userId
+            }
+        });
+
+        if (!favoriteRemoved) {
+            res.status(400);
+            return res.json({ error: "Error in deleting favorite, please try again" });
+        };
+
+        res.status(200);
+        return res.json({ success: "Successfully removed favorite" });
+    }
+    catch (error: any) {
+        res.status(500);
+        return res.json({ error: `Unexpected error occured with error: ${error}` });
+    }
+};
+
+const clearFavorites = async(req: Request, res: Response) => {
+    const userId: string = req.params.userId; // Takes in the User id for deleting all favoirtes
+
+    if (!userId) {
+        res.status(400);
+        return res.json({ error: "No such user exist" });
+    };
+
+    try {
+        const allFavoritesRemoved: number = await Favorites.destroy({
+            where: {
+                userId: userId
+            }
+        })
+
+        if (!allFavoritesRemoved) {
+            res.status(400);
+            return res.json({ error: "Error in deleting all favorites, please try again" });
+        };
+
+        res.status(200);
+        return res.json({ success: "Successfully deleted user Favorites" });
+    }
+    catch (error: any) {
+        res.status(500);
+        return res.json({ error: `Unexpected error occured with error: ${error}` });
+    };
+}
+
 export {
     addFavorite,
-    getFavorites
+    getFavorites,
+    deleteFavorite,
+    clearFavorites
 };
--- server/src/routes/Favorites.ts
+++ server/src/routes/Favorites.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { addFavorite, getFavorites } from "../controllers/Favorites"
+import { addFavorite, deleteFavorite, getFavorites, clearFavorites} from "../controllers/Favorites"
 
 const router: express.Router = express.Router();
 
 router.post('/addFavorite', addFavorite);
 router.get('/getFavorite/:userId', getFavorites);
+router.delete('/delete/:userId/:id', deleteFavorite);
+router.delete('/clear/:userId', clearFavorites)
 
 export default router;
