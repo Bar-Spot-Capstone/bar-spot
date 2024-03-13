05f4d9b2fe6c1cc6fecf536cab4788aadbfcbb5f
DFLotus <deondraefrancis101@gmail.com>
2024-03-13 00:24:17 -0400

    BS-37: Added image icons

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,6 +1,11 @@
 import "../styles/Profile.css"
 import "bootstrap/dist/css/bootstrap.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RiAccountCircleFill } from "react-icons/ri";
+import { MdPrivacyTip } from "react-icons/md";
+import { IoSettingsSharp } from "react-icons/io5";
+import { IoRibbon } from "react-icons/io5";
+
 
 const Profile = () => {
     return (
@@ -11,19 +16,19 @@ const Profile = () => {
                 </div>
                 <div className="header-bar"></div>
                 <div className="row">
-                    <div className="col-6 w-25 pt-3 side-area">
+                    <div className="col-6 col-m-6 w-25 pt-3 side-area">
                         <div className="card card-orientation">
                             <div className="card-body">
-                                <ul>
-                                    <li>Account Setting</li>
-                                    <li>Security & Privacy</li>
-                                    <li>Preferences</li>
-                                    <li>Favorite Bars</li>
+                                <ul className="pt-1">
+                                    <li className="list-element"><RiAccountCircleFill style={{ color: '#0366fc' }} className="icon-style primary" /><button className="btn btn-txt fs-5">Account Setting</button></li>
+                                    <li className="list-element"><MdPrivacyTip style={{ color: '#4c4a52' }} className="icon-style" /><button className="btn btn-txt fs-5">Security & Privacy</button></li>
+                                    <li className="list-element"><IoSettingsSharp className="icon-style" /><button className="btn btn-txt fs-5">Preferences</button></li>
+                                    <li className="list-element"><IoRibbon style={{ color: '#d61c57' }} className="icon-style" /><button className="btn btn-txt fs-5">Favorite Bars</button></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    <div className="col-6 w-75 pt-3 side-content">
+                    <div className="col-6 col-m-6 w-75 pt-3 side-content">
                         <div className="card card-orientation">
                             <div className="card-body">
                                 Side bar Content
@@ -33,7 +38,7 @@ const Profile = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default Profile
