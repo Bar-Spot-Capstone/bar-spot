317523fd04f727a873220e447b627189b03274b5
DFLotus <deondraefrancis101@gmail.com>
2024-03-12 23:45:42 -0400

    BS-37: Added cards

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -10,6 +10,27 @@ const Profile = () => {
                     <h2 className="fs-2">Profile</h2>
                 </div>
                 <div className="header-bar"></div>
+                <div className="row">
+                    <div className="col-6 w-25 pt-3 side-area">
+                        <div className="card card-orientation">
+                            <div className="card-body">
+                                <ul>
+                                    <li>Account Setting</li>
+                                    <li>Security & Privacy</li>
+                                    <li>Preferences</li>
+                                    <li>Favorite Bars</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 w-75 pt-3 side-content">
+                        <div className="card card-orientation">
+                            <div className="card-body">
+                                Side bar Content
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
