e1df4f26b7b6b5f7f41be3eb38204b57e1839670
DFLotus <deondraefrancis101@gmail.com>
2024-04-06 15:53:44 -0400

    BS-44: Invite response and get

--- server/src/controllers/Invitations.ts
+++ server/src/controllers/Invitations.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import Invitation from "../models/Invitations";
 import User from "../models/Users";
+import UserGroup from "../models/UserGroup";
 
 /*
 @param: userId -> retrieves invites based on the userid
@@ -60,6 +61,64 @@ const getAllInvites = async (req: Request, res: Response): Promise<Response> => 
     };
 };
 
+const respondToInvite = async (req: Request, res: Response): Promise<Response> => {
+    const { userId, groupId, response }: { userId: number, groupId: number, response: boolean } = req.body;
+
+    //Should return status 400 if fields are missing
+    var handleEmpty: string = ''
+    handleEmpty = !groupId ? 'groupId' : '' || !userId ? 'ownerId' : '' || response === null ? 'response' : '';
+
+    if (handleEmpty) {
+        res.status(400);
+        return res.json({ error: `Failed to invite user to group, missing field: ${handleEmpty}` });
+    };
+
+    try {
+        if (!response) {
+            //if user does not want to join the usergroup - delete invite that the user has for that groupId
+            const deleteInvite = await Invitation.destroy({
+                where: {
+                    groupId: groupId,
+                    invited_user: userId
+                }
+            });
+
+            if (!deleteInvite) {
+                res.status(400);
+                return res.json({ error: "Failed to reject invitation" });
+            }
+            res.status(200);
+            return res.json({ success: "Rejected invite" });
+        }
+        //if true add to the group and destroy all invites for the user     
+        const joinUserGroup: any = await UserGroup.create({
+            userId: userId,
+            groupId: groupId,
+            role: "member"
+        });
+
+        if (!joinUserGroup) {
+            res.status(400);
+            return res.json({ error: "Failed to join group" });
+        };
+
+        const deleteAllInvites = await Invitation.destroy({
+            where: {
+                invited_user: userId
+            }
+        });
+
+        res.status(200);
+        return res.json({ success: "Joined group" });
+
+    }
+    catch (error: any) {
+        res.status(500);
+        return res.json({ error: `Unexpected error occured with error: ${error}` });
+    };
+};
+
 export {
-    getAllInvites
+    getAllInvites,
+    respondToInvite
 };
--- server/src/routes/Invitations.ts
+++ server/src/routes/Invitations.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllInvites } from "../controllers/Invitations";
+import { getAllInvites, respondToInvite } from "../controllers/Invitations";
 
 const router: express.Router = express.Router();
 
 router.get('/:userId', getAllInvites);
+router.post('/respond', respondToInvite);
 
 export default router;
