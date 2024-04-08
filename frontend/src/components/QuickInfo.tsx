41df796247eb2fbf42ad2a09f0fb6741d1ae4052
SkyMazter <comunidadoscar@gmail.com>
2024-04-07 23:34:21 -0400

    added ascending name sort

--- frontend/src/components/QuickInfo.tsx
+++ frontend/src/components/QuickInfo.tsx
@@ -19,10 +19,85 @@ interface Props {
 
 const QuickInfo = ({ barData }: Props) => {
   const [show, setShow] = useState<boolean>(false);
+  const [sort, setSort] = useState<boolean>(false);
+
+  const handleTestClick = () => {
+    sortByName("ascending");
+  };
+
+  const sortByName = (direction: string) => {
+    if (direction == "ascending") {
+      // let sortingArr: barMenuInfo[] = barData;
+      console.log(barData);
+      // let initialI: number = 0;
+      // console.log(barData.length);
+      mergeSort(barData, 0, barData.length - 1);
+      setSort(true);
+      console.log(barData);
+    }
+  };
+  const merge = (
+    arr: barMenuInfo[],
+    left: number,
+    middle: number,
+    right: number
+  ) => {
+    console.log(arr.length);
+    let size1: number = middle - left + 1;
+    let size2: number = right - middle;
+
+    let array1: barMenuInfo[] = new Array(size1);
+    let array2: barMenuInfo[] = new Array(size2);
+
+    for (let index = 0; index < size1; index++) {
+      array1[index] = arr[left + index];
+    }
+    for (let x = 0; x < size2; x++) {
+      array2[x] = arr[middle + 1 + x];
+    }
+
+    let index1 = 0;
+    let index2 = 0;
+    let mergedIndex = left;
+
+    while (index1 < size1 && index2 < size2) {
+      if (array1[index1].name <= array2[index2].name) {
+        arr[mergedIndex] = array1[index1];
+        index1++;
+      } else {
+        arr[mergedIndex] = array2[index2];
+        index2++;
+      }
+      mergedIndex++;
+    }
+
+    while (index1 < size1) {
+      arr[mergedIndex] = array1[index1];
+      index1++;
+      mergedIndex++;
+    }
+    while (index2 < size2) {
+      arr[mergedIndex] = array2[index2];
+      index2++;
+      mergedIndex++;
+    }
+  };
+
+  const mergeSort = (arr: barMenuInfo[], left: number, right: number) => {
+    if (left >= right) {
+      return;
+    }
+
+    let middle: number = left + Math.floor((right - left) / 2);
+
+    mergeSort(arr, left, middle);
+    mergeSort(arr, middle + 1, right);
+    merge(arr, left, middle, right);
+  };
 
   useEffect(() => {
     setShow(true);
-  }, [barData]);
+  }, [sort]);
 
   if (show) {
     return (
@@ -35,7 +110,12 @@ const QuickInfo = ({ barData }: Props) => {
                 <ListGroup.Item className="d-flex justify-content-around align-items-center">
                   Sort By Name:
                   <ButtonGroup>
-                    <Button variant="outline-secondary">A-Z</Button>
+                    <Button
+                      variant="outline-secondary"
+                      onClick={handleTestClick}
+                    >
+                      A-Z
+                    </Button>
                     <Button variant="outline-secondary">Z-A</Button>
                   </ButtonGroup>
                 </ListGroup.Item>
