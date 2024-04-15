03bac16c79bba4f20d200133b41cb2c96d97dd70
DFLotus <deondraefrancis101@gmail.com>
2024-04-14 21:39:44 -0400

    BS-48: Updated base name

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element:<MapView/>,
+        element: <MapView />,
       },
       {
         path: "/profile",
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
     path: "/sign-up",
     element: <CreateNewAcc />,
   },
-]);
+], { basename: '/' });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
