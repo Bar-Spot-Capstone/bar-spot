ddc82a9fc11b0be928321646b2efcd05e5ebb94d
DFLotus <deondraefrancis101@gmail.com>
2024-04-23 18:45:49 -0400

    BS-47: Added middlewear to routes

--- server/src/routes/Invitations.ts
+++ server/src/routes/Invitations.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { getAllInvites, respondToInvite } from "../controllers/Invitations";
+import { verifyToken } from "../controllers/User";
 
 const router: express.Router = express.Router();
 
-router.get('/:userId', getAllInvites);
-router.post('/respond', respondToInvite);
+router.get('/:userId', verifyToken, getAllInvites);
+router.post('/respond', verifyToken, respondToInvite);
 
 export default router;
--- server/src/routes/User.ts
+++ server/src/routes/User.ts
@@ -5,7 +5,7 @@ const router: express.Router = express.Router();
 
 router.post('/register', userRegister);
 router.post('/login', userLogin);
-router.delete('/delete/:id', deleteUser);
+router.delete('/delete/:id', verifyToken, deleteUser);
 router.get('/all/:id', getUsers);
 
 export default router;
