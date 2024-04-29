1ee07fdb57e1e2a0a9ea31025b023df8a08e8e92
Miles Carden <milescarden@gmail.com>
2024-04-29 18:14:51 -0400

    fixed from local testing back to normal

--- frontend/src/types/fetchCall.tsx
+++ frontend/src/types/fetchCall.tsx
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:3001";
+const baseURL = "https://barspot-server.onrender.com";
 
 const partyCreate = `${baseURL}/party/create`;
 const userRegister = `${baseURL}/user/register`;
